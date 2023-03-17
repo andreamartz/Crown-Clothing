c754ce91f1c74d95e119a6ab8b4902d232cbe550
Andrea Martz <14165788+andreamartz@users.noreply.github.com>
2023-03-17 12:16:55 -0500

    Build and style navigation links with appropriate routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 
-import Navigation from './routes/navigation/navigation.component';
 import Home from './routes/home/home.component';
+import Navigation from './routes/navigation/navigation.component';
 
 const Shop = () => {
   return <h1>I am the shop page</h1>;
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,10 +1,21 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, Link } from 'react-router-dom';
+
+import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
+
+import './navigation.styles.scss';
 
 const Navigation = () => {
   return (
     <>
-      <div>
-        <h1>I am the navigation bar</h1>
+      <div className='navigation'>
+        <Link className='logo-container' to='/'>
+          <CrwnLogo className='logo' />
+        </Link>
+        <div className='nav-links-container'>
+          <Link className='nav-link' to='/shop'>
+            SHOP
+          </Link>
+        </div>
       </div>
       <Outlet />
     </>
