734ab180a0b18fc7fdebfb91c42407f15e3e72dd
Andrea Martz <14165788+andreamartz@users.noreply.github.com>
2023-03-18 20:26:58 -0500

    Create sign up form

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -8,6 +8,7 @@ import {
   signInWithRedirect, 
   signInWithPopup, 
   GoogleAuthProvider,
+  createUserWithEmailAndPassword,
 } from 'firebase/auth';
 import {  
   getFirestore,
@@ -65,6 +66,12 @@ export const createUserDocumentFromAuth = async (userAuth) => {
     }
   }
 
-// if user data exists
+  // if user data exists
   return userDocRef;
 };
+
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
+  if (!email || !password) return;
+
+  return await createUserWithEmailAndPassword(auth, email, password);
+}
